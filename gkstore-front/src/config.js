e337e687d29b9af818dbc93a1aa27c51753621d8
unknown <itsworkingmind@gmail.com>
2020-09-08 12:33:54 +0530

    first commit

---
+++ gkstore-front/src/config.js
@@ -0,0 +1,4 @@
+// export const API = process.env.REACT_APP_API_URL;
+// export const API = `http://localhost:${process.env.PORT}/api`;
+
+export const API = "https://firstheroku-store.herokuapp.com/api";
